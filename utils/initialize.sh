83761bdfdd2cb750e60de8498565f0659459a252
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-21 10:15:37 +0200

    creatin keystores....

--- utils/initialize.sh
+++ utils/initialize.sh
@@ -26,6 +26,8 @@ openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certifi
 openssl pkcs12 -inkey key.pem -in certificate.pem -export -out certificate.p12 -passout pass:
 #openssl pkcs12 -in certificate.p12 -noout -info
 
+mkdir -p /home/alpine/.pki/nssdb
+sudo -u alpine certutil -d sql:/home/alpine/.pki/nssdb --empty-password
 sudo -u alpine certutil -d sql:/home/alpine/.pki/nssdb -A -t "CT,c,c" -n /usr/local/share/ca-certificates/AutoFirma_ROOT.crt -i /usr/local/share/ca-certificates/AutoFirma_ROOT.crt
 sudo -u alpine pk12util -d sql:/home/alpine/.pki/nssdb -i certificate.p12 -W ""
 
