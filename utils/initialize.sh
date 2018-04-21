a0ac1d675fa8383535f1e52c5b8a2969007e0440
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-21 09:33:43 +0200

    force dir creations

--- utils/initialize.sh
+++ utils/initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir /tmp/autofirma
+mkdir -p /tmp/autofirma
 cd /tmp/autofirma
 wget http://estaticos.redsara.es/comunes/autofirma/currentversion/AutoFirma_Linux.zip
 unzip AutoFirma_Linux.zip
@@ -19,7 +19,7 @@ dos2unix autofirma_policy.json
 cp /tmp/utils/AutoFirma /usr/bin/AutoFirma
 chmod 777 /usr/bin/AutoFirma
 
-mkdir /tmp/certs
+mkdir -p /tmp/certs
 cd /tmp/certs
 openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem < /tmp/utils/certInfo.txt
 #openssl x509 -text -noout -in certificate.pem
@@ -29,7 +29,7 @@ openssl pkcs12 -inkey key.pem -in certificate.pem -export -out certificate.p12 -
 sudo -u alpine certutil -d sql:/home/alpine/.pki/nssdb -A -t "CT,c,c" -n /usr/local/share/ca-certificates/AutoFirma_ROOT.crt -i /usr/local/share/ca-certificates/AutoFirma_ROOT.crt
 sudo -u alpine pk12util -d sql:/home/alpine/.pki/nssdb -i certificate.p12 -W ""
 
-mkdir /etc/opt/chrome/policies/managed
+mkdir -p /etc/opt/chrome/policies/managed
 chmod -w /etc/opt/chrome/policies/managed
 cp /tmp/utils/autofirma_policy.json /etc/opt/chrome/policies/managed/autofirma_policy.json
 
